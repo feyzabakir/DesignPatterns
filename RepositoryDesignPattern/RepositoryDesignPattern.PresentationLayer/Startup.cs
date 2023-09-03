fbc792c5b594a14550eb00a765519f4b7857c9a4
Feyza Nur Bakır <120409251+feyzabakir@users.noreply.github.com>
2023-09-04 01:03:54 +0300

    Repository Design Pattern

--- RepositoryDesignPattern/RepositoryDesignPattern.PresentationLayer/Startup.cs
+++ RepositoryDesignPattern/RepositoryDesignPattern.PresentationLayer/Startup.cs
@@ -5,7 +5,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RepositoryDesignPattern.BusinessLayer.Abstract;
+using RepositoryDesignPattern.BusinessLayer.Concrete;
+using RepositoryDesignPattern.DataAccessLayer.Abstract;
 using RepositoryDesignPattern.DataAccessLayer.Concrete;
+using RepositoryDesignPattern.DataAccessLayer.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +29,9 @@ namespace RepositoryDesignPattern.PresentationLayer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ICategoryService, CategoryManager>();
+            services.AddScoped<ICategoryDal, EfCategoryDal>();
+
             services.AddEntityFrameworkNpgsql().AddDbContext<Context>(opt =>
   opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
