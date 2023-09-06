fdd3d7c0c2ecbf7ea5fc5f2c3aee1f3ecfde47a9
Feyza Nur Bakır <120409251+feyzabakir@users.noreply.github.com>
2023-09-06 23:02:42 +0300

    Facade Design Pattern

---
+++ FacadeDesignPattern/DesignPattern.Facade/DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Facade.DAL
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-4OD0B581;initial catalog=DesignPattern10; integrated security=true;");
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
