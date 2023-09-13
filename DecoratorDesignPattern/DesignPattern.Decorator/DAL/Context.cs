88f564059001323c5df9b3a4fa6a75bd02a32ab3
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2023-09-13 13:17:24 +0300

    Decorator Design Pattern

---
+++ DecoratorDesignPattern/DesignPattern.Decorator/DAL/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Decorator.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("server=LAPTOP-4OD0B581;initial catalog=DesignPattern11; integrated security=true;");
+
+        }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Notifier> Notifiers { get; set; }
+    }
+}
---
+++ DecoratorDesignPattern/DesignPattern.Decorator/Migrations/20230913090254_mig1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DesignPattern.Decorator.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessageID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MessageSender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MessageReceiver = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MessageSubject = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MessageContent = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessageID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notifiers",
+                columns: table => new
+                {
+                    NotifierID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NotifierCreator = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NotifierSubject = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NotifierType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NotifierChannel = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifiers", x => x.NotifierID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Notifiers");
+        }
+    }
+}
