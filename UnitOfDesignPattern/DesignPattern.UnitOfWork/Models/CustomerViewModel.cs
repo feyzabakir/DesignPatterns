a3d5bc25add208ecb19bcd89a493fdf0cd5f5807
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2023-09-15 22:57:30 +0300

    Unit Of Work Design Pattern

---
+++ UnitOfDesignPattern/DesignPattern.UnitOfWork/Models/CustomerViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPattern.UnitOfWork.Models
+{
+    public class CustomerViewModel
+    {
+        public int SenderID { get; set; }
+        public int ReceiverID { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
